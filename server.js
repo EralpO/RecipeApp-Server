6761633d0a339167dd5770ec9dc932b7cd9ab3b5
EralpO <eralponay10@gmail.com>
2020-08-02 23:59:45 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const connectDB = require('./config/db')
+const app = express();
+
+//Connect Database
+
+connectDB();
+
+//Init bodyparser
+
+app.use(express.json({extended:false}))
+
+const port = process.env.PORT || 8080 ;
+
+
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+
+app.listen(port , ()=>{
+    console.log(`Server started at ${port}`)
+});

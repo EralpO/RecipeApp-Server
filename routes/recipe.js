229d7dbd574eecad6ac10fa2026f288e5acdd00a
EralpO <eralponay10@gmail.com>
2020-08-16 17:14:01 +0300

    recipe router

--- routes/recipe.js
+++ routes/recipe.js
@@ -51,4 +51,43 @@ router.post(
   }
 );
 
+router.get('/list',async (req,res)=>{
+    try {
+        let recipeList = await Recipe.find({})
+        if(!recipeList)
+           res.status(404).json({msg:"There are currently no recipes exist"})
+        res.status(200).json(recipeList)
+    } catch (error) {
+        res.status(500).json({msg:"Server Error"})
+    }
+  
+})
+
+router.get("/list/:id",async (req,res)=>{
+    try {
+        let id = req.params.id.trim()
+        console.log(id)
+        let recipe =await Recipe.findOne({_id:id})
+        console.log(recipe)
+        if(!recipe)
+        res.status(404).json({msg:"There is no recipe with id of "+id})
+        res.status(200).json(recipe)
+    } catch (error) {
+        res.status(500).json({msg:"Server Error"})
+    }
+})
+
+
+router.get("/listbycategory/:id",async (req,res)=>{
+    try {
+        let id = req.params.id.trim()
+        let recipe = await Recipe.findOne({category:id})
+        if(!recipe)
+        res.status(404).json({msg:"There is no recipe in that category"})
+        res.status(200).json(recipe)
+    } catch (error) {
+        res.status(500).json({msg:"Server Error"})
+    }
+})
+
 module.exports = router;
